0d69578be2e311976a972fb1fc3d1e5f70d6be70
Cody C <codyicastro@gmail.com>
2020-06-23 04:57:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" ", " ", "  ", "  ", "  ", " ", " ", " ", " "]
+#rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+
+
+
+def display_board(ttt)
+  puts " #{ttt[0]} | #{ttt[1]} | #{ttt[2]} "
+  puts "-----------"
+  puts " #{ttt[3]} | #{ttt[4]} | #{ttt[5]} "
+  puts "-----------"
+  puts " #{ttt[6]} | #{ttt[7]} | #{ttt[8]} "
+end
+
+display_board(board)
+
+
+
